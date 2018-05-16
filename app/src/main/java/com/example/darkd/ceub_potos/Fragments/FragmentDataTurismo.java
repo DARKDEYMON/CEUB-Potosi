9307577e3fa2c994be18828507840aaa1b47cdb8
darkdeymon <darkdeymon04@gmail.com>
2018-05-16 17:50:18 -0400

    v10

--- app/src/main/java/com/example/darkd/ceub_potos/Fragments/FragmentDataTurismo.java
+++ app/src/main/java/com/example/darkd/ceub_potos/Fragments/FragmentDataTurismo.java
@@ -8,22 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.darkd.ceub_potos.R;
 import com.example.darkd.ceub_potos.adapters.AdapterEntretenimiento;
 import com.example.darkd.ceub_potos.adapters.AdapterRestaurantes;
 import com.example.darkd.ceub_potos.adapters.AdapterTurismo;
+import com.example.darkd.ceub_potos.clases.Hoteles;
 import com.example.darkd.ceub_potos.clases.Restaurantes;
 import com.example.darkd.ceub_potos.clases.Turismo;
 import com.example.darkd.ceub_potos.staticDataClases.TurismoData;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentDataTurismo extends Fragment {
+public class FragmentDataTurismo extends Fragment implements AdapterView.OnItemClickListener {
 
     private ArrayList<Turismo> arTurismo;
     private AdapterTurismo adTurismo;
@@ -47,5 +50,19 @@ public class FragmentDataTurismo extends Fragment {
         adTurismo = new AdapterTurismo(getActivity(),arTurismo);
         lstTurismo = (ListView)view.findViewById(R.id.listaturismo);
         lstTurismo.setAdapter(adTurismo);
+        lstTurismo.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Turismo t = (Turismo)parent.getAdapter().getItem(position);
+        Bundle b = new Bundle();
+        b.putString("turismo",new Gson().toJson(t));
+        FragmentDetailsTurismo nextFrag= new FragmentDetailsTurismo();
+        nextFrag.setArguments(b);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.contenedor, nextFrag)
+                .addToBackStack(null)
+                .commit();
     }
 }
